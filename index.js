cae9b8fd0bf169fd5c8643739594178639e5a72f
Suna <minsuna63@gmail.com>
2023-09-25 19:13:46 +0600

    added text

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-let sunita = document.querySelector('.sunita');
+const likeButton = document.getElementById('likeButton');
+const dislikeButton = document.getElementById('dislikeButton');
+const element = document.getElementById('element');
+
+
+const original = element.style.background;
+
+dislikeButton.addEventListener('click', () => {
+    element.style.display = 'none';
+    const newEl = document.createElement('div');
+    newEl.textContent = 'Ты просто не шаришь';
+    element.parentNode.appendChild(newEl);
+});
+
+likeButton.addEventListener('click', () => {
+    element.style.background = 'black';
+    setTimeout(() => {
+        element.style.background = original;
+    }, 3000);
+});
